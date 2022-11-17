969821cc58ca2e3b931124b65b13731717f48c91
Inegreira <100797498+Inegreira@users.noreply.github.com>
2022-11-17 17:10:35 +0100

    .

--- Tamizjo/app/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(realpath(dirname(__FILE__) . '/..') . '/config/config.php');
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
